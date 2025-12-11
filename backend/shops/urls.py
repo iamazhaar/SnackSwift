ac4561f866eea535029ad21a142517abe8e1bd52
Mazharul Islam <abracadabraamm@gmail.com>
2025-12-11 20:00:18 +0600

    ShopOwner-Management-URLs-Added

--- backend/shops/urls.py
+++ backend/shops/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import ShopListView, ShopDetailView, ShopMenuListView
+from .views import ShopListView, ShopDetailView, ShopMenuListView, ManageMenuCreateView, ManageMenuDetailView
 
 
 urlpatterns = [
+    # Public / Student URLs
     path('', ShopListView.as_view(), name='shop-list'),
     path('<int:id>/', ShopDetailView.as_view(), name='shop-detail'),
-    path('<int:id>/menu/', ShopMenuListView.as_view(), name='shop-menu')
+    path('<int:id>/menu/', ShopMenuListView.as_view(), name='shop-menu'),
+
+    # Shop Owner Management URLs
+    path('manage/menu/add/', ManageMenuCreateView.as_view(), name='manage-menu-add'),
+    path('manage/menu/<int:pk>/', ManageMenuDetailView.as_view(), name='manage-menu-edit'),
 ]
