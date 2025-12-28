08572b7d109ed079e4cccbb835bc7336f8e7bcbe
Mazharul Islam <abracadabraamm@gmail.com>
2025-12-29 01:27:39 +0600

    Dashboard Layout Modified

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -1,12 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
+import DashboardHeader from '../components/DashboardHeader'
 
 const DashboardLayout = () => {
   return (
-    <div style={{ display: "flex" }}>
-      <Sidebar /> {/* The Side Menu */}
-      <div style={{ marginLeft: "260px", width: "100%", padding: "2rem", background: "#f9fafb", minHeight: "100vh" }}>
-        <Outlet /> {/* This renders Dashboard, Menu, etc. */}
+    <div style={{ display: "flex", backgroundColor: "#f9fafb", minHeight: "100vh" }}>
+      {/* 1. Sidebar Fixed Left */}
+      <Sidebar />
+
+      {/* 2. Main Content Area */}
+      <div style={{ 
+          marginLeft: "250px", // Matches Sidebar width
+          width: "100%", 
+          padding: "0 2rem 2rem 2rem" // Padding for the content
+      }}>
+        {/* A. The Top Header */}
+        <DashboardHeader />
+
+        {/* B. The Page Content (Dashboard, Menu, etc.) */}
+        <Outlet />
       </div>
     </div>
   );
